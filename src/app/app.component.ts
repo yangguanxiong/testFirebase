267799162bd017351680336866d596684ca7e796
Guanxiong <gyang@ezesoft.com>
2016-10-28 15:14:40 -0400

    trying to retrive data from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+  <h1>{{ item | async }}</h1>
+  {{item.$key}}
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+
+  item: FirebaseObjectObservable<any>;
+  constructor(af: AngularFire) {
+    this.item = af.database.object('https://testfirebase-75469.firebaseio.com/test1');
+    console.log(this.item);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBtzMmq1zICAufZFHppp6jn0uD13sU659k',
+  authDomain: 'testfirebase-75469.firebaseapp.com',
+  databaseURL: 'https://testfirebase-75469.firebaseio.com/',
+  storageBucket: 'testfirebase-75469.appspot.com'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
